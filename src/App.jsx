249f28e2934c65702d3a73a3cca60b69ac897c1d
Faysal <74490913+faysalchowdhury99@users.noreply.github.com>
2021-05-13 00:46:39 +0600

    Learn React

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 import Lesson1 from './Lessons/Lesson-1/JSX-Element-and-Rendering';
 import Lesson2 from './Lessons/Lesson-2/Component-and-Props';
 import Lesson3 from './Lessons/Lesson-3/State-and-Lifecycle-in-Class-Component';
-import Lesson4 from './Lessons/Lesson-4/Handling-Events--Detect-and-Control-Unnecessary-Re-Render';
+import Lesson4 from './Lessons/Lesson-4/Handling-Events--Detect-and-Control-Unnecessary-Re-Renders';
 
 function App() {
   const [count, setCount] = useState(0);
