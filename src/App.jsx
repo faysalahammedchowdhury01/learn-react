bfaf31d983543c528720fb13249498542ec8ac5c
Faysal <74490913+faysalchowdhury99@users.noreply.github.com>
2021-05-13 14:22:24 +0600

    Learn React

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import './App.css';
 /* import Lesson1 from './Lessons/Lesson-1/JSX-Element-and-Rendering';
 import Lesson2 from './Lessons/Lesson-2/Component-and-Props';
 import Lesson3 from './Lessons/Lesson-3/State-and-Lifecycle-in-Class-Component';
-import Lesson4 from './Lessons/Lesson-4/Handling-Events--Detect-and-Control-Unnecessary-Re-Renders'; 
-import Lesson5 from './Lessons/Lesson-5/Conditional-Rendering'; */
+import Lesson4 from './Lessons/Lesson-4/Handling-Events--Detect-and-Control-Unnecessary-Re-Renders';
+import Lesson5 from './Lessons/Lesson-5/Conditional-Rendering';
+import Lesson6 from './Lessons/Lesson-6/List-and-Keys'; */
 
 function App() {
   const [count, setCount] = useState(0);
+  // const quantities = [1, 2, 3, 4, 5]; // lesson6
   return (
     <div className="App">
       <h1>Bismillahir Rahmanir Rahim</h1>
@@ -15,7 +17,8 @@ function App() {
       <Lesson2 />
       <Lesson3 locale="bn-BD" />
       <Lesson4 /> 
-      <Lesson5 /> */}
+      <Lesson5 /> 
+      <Lesson6 quantities={quantities} />*/}
     </div>
   );
 }
