6aec8380e845f41b8f2abca0ba87c1f7633c8193
Faysal <74490913+faysalchowdhury99@users.noreply.github.com>
2021-05-18 23:50:44 +0600

    Learn React

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,9 @@ import Lesson3 from './Lessons/Lesson-3/State-and-Lifecycle-in-Class-Component';
 import Lesson4 from './Lessons/Lesson-4/Handling-Events--Detect-and-Control-Unnecessary-Re-Renders';
 import Lesson5 from './Lessons/Lesson-5/Conditional-Rendering';
 import Lesson6 from './Lessons/Lesson-6/List-and-Keys';
-import Lesson7 from './Lessons/Lesson-7/Form-Handling';*/
-import Lesson8 from './Lessons/Lesson-8/Calculator';
+import Lesson7 from './Lessons/Lesson-7/Form-Handling';
+import Lesson8 from './Lessons/Lesson-8/Lesson8';*/
+import Lesson9 from './Lessons/Lesson-9/Lesson9';
 
 function App() {
   // const quantities = [1, 2, 3, 4, 5]; // lesson6
@@ -20,8 +21,9 @@ function App() {
       <Lesson4 /> 
       <Lesson5 /> 
       <Lesson6 quantities={quantities} />
-      <Lesson7 />*/}
-      <Lesson8 />
+      <Lesson7 />
+      <Lesson8 />*/}
+      <Lesson9 />
     </div>
   );
 }
