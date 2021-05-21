fe50b1bffaf67343b9e80dee3e92e313c55a2ed0
Faysal <74490913+faysalchowdhury99@users.noreply.github.com>
2021-05-22 00:36:03 +0600

    Learn React

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,9 @@ import Lesson8 from './Lessons/Lesson-8/Lesson8';
 import Lesson9 from './Lessons/Lesson-9/Lesson9';
 import Lesson10 from './Lessons/Lesson-10/Lesson10';
 import Lesson11 from './Lessons/Lesson-11/Lesson11';
-import Lesson12 from './Lessons/Lesson-12/Lesson12';*/
-import Lesson13 from './Lessons/Lesson-13/Lesson13';
+import Lesson12 from './Lessons/Lesson-12/Lesson12';
+import Lesson13 from './Lessons/Lesson-13/Lesson13';*/
+import Lesson14 from './Lessons/Lesson-14/Lesson14';
 
 function App() {
   // const quantities = [1, 2, 3, 4, 5]; // lesson6
@@ -30,8 +31,9 @@ function App() {
       <Lesson9 />
       <Lesson10 />
       <Lesson11 />
-      <Lesson12 />*/}
-      <Lesson13 />
+      <Lesson12 />
+      <Lesson13 />*/}
+      <Lesson14 />
     </div>
   );
 }
