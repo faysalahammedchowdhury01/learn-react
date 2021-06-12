632b149ee94eba30dcf17c8f603801b877a66fa6
Faysal <74490913+faysalchowdhury99@users.noreply.github.com>
2021-06-12 11:23:06 +0600

    Learn React.memo(), useCallback, useMemo Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,40 @@
 import React from 'react';
 import './App.css';
-/* import Lesson1 from './Lessons/Lesson-1/JSX-Element-and-Rendering';
-import Lesson2 from './Lessons/Lesson-2/Component-and-Props';
-import Lesson3 from './Lessons/Lesson-3/State-and-Lifecycle-in-Class-Component';
-import Lesson4 from './Lessons/Lesson-4/Handling-Events--Detect-and-Control-Unnecessary-Re-Renders';
-import Lesson5 from './Lessons/Lesson-5/Conditional-Rendering';
-import Lesson6 from './Lessons/Lesson-6/List-and-Keys';
-import Lesson7 from './Lessons/Lesson-7/Form-Handling';
-import Lesson8 from './Lessons/Lesson-8/Lesson8';
-import Lesson9 from './Lessons/Lesson-9/Lesson9';
-import Lesson10 from './Lessons/Lesson-10/Lesson10';
-import Lesson11 from './Lessons/Lesson-11/Lesson11';
-import Lesson12 from './Lessons/Lesson-12/Lesson12';
-import Lesson13 from './Lessons/Lesson-13/Lesson13';*/
-import Lesson14 from './Lessons/Lesson-14/Lesson14';
+// import Lesson1 from './Lessons/Lesson-1/JSX-Element-and-Rendering';
+// import Lesson2 from './Lessons/Lesson-2/Component-and-Props';
+// import Lesson3 from './Lessons/Lesson-3/State-and-Lifecycle-in-Class-Component';
+// import Lesson5 from './Lessons/Lesson-5/Conditional-Rendering';
+// import Lesson6 from './Lessons/Lesson-6/List-and-Keys';
+// import Lesson7 from './Lessons/Lesson-7/Form-Handling';
+// import Lesson8 from './Lessons/Lesson-8/Lesson8';
+// import Lesson9 from './Lessons/Lesson-9/Lesson9';
+// import Lesson10 from './Lessons/Lesson-10/Lesson10';
+// import Lesson11 from './Lessons/Lesson-11/Lesson11';
+// import Lesson12 from './Lessons/Lesson-12/Lesson12';
+// import Lesson13 from './Lessons/Lesson-13/Lesson13';
+// import Lesson14 from './Lessons/Lesson-14/Lesson14';
+import Lesson15 from './Lessons/Lesson-15/Lesson15';
 
 function App() {
   // const quantities = [1, 2, 3, 4, 5]; // lesson6
   return (
     <div className="App">
       <h1>Bismillahir Rahmanir Rahim</h1>
-      {/* <Lesson1 />
-      <Lesson2 />
-      <Lesson3 locale="bn-BD" />
-      <Lesson4 /> 
-      <Lesson5 /> 
-      <Lesson6 quantities={quantities} />
-      <Lesson7 />
-      <Lesson8 />
-      <Lesson9 />
-      <Lesson10 />
-      <Lesson11 />
-      <Lesson12 />
-      <Lesson13 />*/}
-      <Lesson14 />
+      {/* <Lesson1 />*/}
+      {/* <Lesson2 /> */}
+      {/* <Lesson3 locale="bn-BD" /> */}
+      {/* <Lesson4 /> */}
+      {/* <Lesson5 />  */}
+      {/* <Lesson6 quantities={quantities} /> */}
+      {/* <Lesson7 /> */}
+      {/* <Lesson8 /> */}
+      {/* <Lesson9 /> */}
+      {/* <Lesson10 /> */}
+      {/* <Lesson11 /> */}
+      {/* <Lesson12 /> */}
+      {/* <Lesson13 /> */}
+      {/* <Lesson14 /> */}
+      <Lesson15 />
     </div>
   );
 }
